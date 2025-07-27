c88763aa97ed1c22709d789a92dfa39b4a9ec69f
Julian Sukrisna <smallest87@gmail.com>
2025-07-27 17:20:28 +0700

    Bilah modular
    membuat tiap bilah menjadi lebih modular

---
+++ src/Views/partials/sidebar_right.php
@@ -0,0 +1,16 @@
+<?php
+// views/partials/sidebar_right.php
+?>
+<div class="right-sidebar">
+    <h2>Topik Populer</h2>
+    <ul>
+        <li><a href="#"><span class="icon">&#128172;</span> Berita Terbaru</a></li>
+        <li><a href="#"><span class="icon">&#128218;</span> Artikel Terpopuler</a></li>
+        <li><a href="#"><span class="icon">&#128188;</span> Karir & Pekerjaan</a></li>
+        <li><a href="#"><span class="icon">&#128161;</span> Tips & Trik</a></li>
+        <li><a href="#"><span class="icon">&#128240;</span> Teknologi</a></li>
+        <li><a href="#"><span class="icon">&#127760;</span> Lingkungan</a></li>
+        <li><a href="#"><span class="icon">&#127918;</span> Hiburan</a></li>
+        <li><a href="#"><span class="icon">&#128200;</span> Ekonomi</a></li>
+    </ul>
+</div>
